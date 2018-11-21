09c7701c7be7ad1976d80dc6260a08a76cf73776
Nguyen Thi Yen <nguyenyen2003972gmail.com>
2018-11-20 17:38:53 -0800

    init project

---
+++ config/application.rb
@@ -0,0 +1,12 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.i18n.default_locale = :en
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+	scope "(:locale)", locale: /en|vi/ do
+		get 'static_pages/help'
+		get "static_pages/home", to: "static_pages#home"
+		get  'static_pages/about'
+		get  'static_pages/contact'
+		root 'static_pages#home'
+	end
+end

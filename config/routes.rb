d3d67f1a0ce67ec7487d5dfdf8b4837ff2f6bd6c
Nguyen Yen <nguyenyen200397@gmail.com>
2018-11-26 08:13:04 +0700

    add file chap4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	scope "(:locale)", locale: /en|vi/ do
+		get 'static_pages/help'
+		get "static_pages/home", to: "static_pages#home"
+		root 'static_pages#home'
+	end
 end
